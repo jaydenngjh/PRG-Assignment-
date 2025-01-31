f5ec93d73812eca42fb6ab0c814213bb4d7f2647
jsupanedu <167403910+jsupanedu@users.noreply.github.com>
2025-02-01 00:44:01 +0800

    Update Flight.cs

--- PRG ASST/Flight.cs
+++ PRG ASST/Flight.cs
@@ -1,53 +1,44 @@
-//==========================================================
-// Student Number: S10268227
-// Student Name: Justine Kyle Supan
-// Partner Name: Jayden Ng
-//==========================================================
+using System;
 
-namespace PRG_ASST
+public class Flight : IComparable<Flight>
 {
-    public class Flight : IComparable<Flight>
+    public string FlightNumber { get; set; }
+    public string Origin { get; set; }
+    public string Destination { get; set; }
+    public DateTime ExpectedTime { get; set; }
+    public string Status { get; set; }
+
+    public Flight(string flightNumber, string origin, string destination, DateTime expectedTime, string status)
     {
-        public string FlightNumber { get; set; }
-        public string Origin { get; set; }
-        public string Destination { get; set; }
-        public DateTime ExpectedTime { get; set; }
-        public string Status { get; set; }
+        FlightNumber = flightNumber;
+        Origin = origin;
+        Destination = destination;
+        ExpectedTime = expectedTime;
+        Status = status;
+    }
 
-
-        public Flight(string flightNumber, string origin, string destination, DateTime expectedTime, string status)
+    public double CalculateFees()
+    {
+        double fee = 0;
+        if (Destination == "Singapore (SIN)") // Arriving flight
         {
-            FlightNumber = flightNumber;
-            Origin = origin;
-            Destination = destination;
-            ExpectedTime = expectedTime;
-            Status = status;
+            fee += 500;
         }
 
-        public double CalculateFees()
+        else if (Origin == "Singapore (SIN)") // Departing flight
         {
-            double fee = 0;
-            if (Destination == "Singapore (SIN)") // Arriving flight
-            {
-                fee += 500;
-            }
-
-            else if (Origin == "Singapore (SIN)") // Departing flight
-            {
-                fee += 800;
-            }
-            return fee;
+            fee += 800;
         }
+        return fee;
+    }
 
-        public override string ToString()
-        {
-            return $"Flight: {FlightNumber}, Origin: {Origin}, Destination: {Destination}, ExpectedTime: {ExpectedTime}, Status: {Status}";
-        }
+    public override string ToString()
+    {
+        return $"Flight: {FlightNumber}, Origin: {Origin}, Destination: {Destination}, ExpectedTime: {ExpectedTime}, Status: {Status}";
+    }
 
-        public int CompareTo(Flight other)
-        {
-            return this.ExpectedTime.CompareTo(other.ExpectedTime);
-        }
-
+    public int CompareTo(Flight other)
+    {
+        return this.ExpectedTime.CompareTo(other.ExpectedTime);
     }
 }
