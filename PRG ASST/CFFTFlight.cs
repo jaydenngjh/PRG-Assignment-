474e28b4c82c5ac4beafeaea504b0cda1024d1d8
jsupanedu <167403910+jsupanedu@users.noreply.github.com>
2025-02-01 00:44:36 +0800

    Update CFFTFlight.cs

--- PRG ASST/CFFTFlight.cs
+++ PRG ASST/CFFTFlight.cs
@@ -4,9 +4,7 @@
 // Partner Name: Jayden Ng
 //==========================================================
 
-namespace PRG_ASST
-{
-    class CFFTFlight : Flight
+class CFFTFlight : Flight
     {
         public double RequestFee { get; set; }
         public CFFTFlight(string flightNumber, string origin, string destination, DateTime expectedTime, string status) : base(flightNumber, origin, destination, expectedTime, status)
@@ -22,4 +20,3 @@ namespace PRG_ASST
             return base.ToString();
         }
     }
-}
