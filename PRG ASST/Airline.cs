537258aa0186b27d84ffefd70abaf5518a34a74e
jsupanedu <167403910+jsupanedu@users.noreply.github.com>
2025-02-01 00:08:28 +0800

    Update Airline.cs - Justine
    - Changed constructor to not include the dictionary

--- PRG ASST/Airline.cs
+++ PRG ASST/Airline.cs
@@ -1,55 +1,44 @@
-//==========================================================
-// Student Number: S10268227
-// Student Name: Justine Kyle Supan
-// Partner Name: Jayden Ng
-//==========================================================
+using System;
+using System.Collections.Generic;
 
-namespace PRG_ASST
+public class Airline
 {
-    public class Airline
+    public string Name { get; set; }
+    public string Code { get; set; }
+    public Dictionary<string, Flight> Flights { get; set; }
+
+    public Airline(string name, string code)
     {
-        public string Name { get; set; }
-        public string Code { get; set; }
-        public Dictionary<string, Flight> Flights { get; set; }
+        Name = name;
+        Code = code;
+    }
 
-        public Airline(string name, string code)
-        {
-            Name = name;
-            Code = code;
-            Flights = new Dictionary<string, Flight>();
-        }
+    public bool AddFlight(Flight flight)
+    {
+        if (Flights.ContainsKey(flight.FlightNumber))
+            return false;
 
-        public bool AddFlight(Flight flight)
-        {
-            if (Flights.ContainsKey(flight.FlightNumber))
-            {
-                return false;
-            }
-            else
-            {
-                Flights.Add(flight.FlightNumber, flight);
-                return true;
-            }
-        }
+        Flights.Add(flight.FlightNumber, flight);
+        return true;
+    }
 
-        public double CalculateFees()
+    public double CalculateFees()
+    {
+        double totalFees = 0;
+        foreach (var flight in Flights.Values)
         {
-            double totalFees = 0;
-            foreach (var flight in Flights.Values)
-            {
-                totalFees += flight.CalculateFees();
-            }
-            return totalFees;
+            totalFees += flight.CalculateFees();
         }
+        return totalFees;
+    }
 
-        public bool RemoveFlight(Flight flight)
-        {
-            return Flights.Remove(flight.FlightNumber);
-        }
+    public bool RemoveFlight(Flight flight)
+    {
+        return Flights.Remove(flight.FlightNumber);
+    }
 
-        public override string ToString()
-        {
-            return $"Airline: {Name}, Code: {Code}, Flights: {Flights.Count}";
-        }
+    public override string ToString()
+    {
+        return $"Airline: {Name}, Code: {Code}, Flights: {Flights.Count}";
     }
 }
