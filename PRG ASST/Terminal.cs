b5d25b38ddf8157d6d13c722e7a4354af7d71ace
jsupanedu <167403910+jsupanedu@users.noreply.github.com>
2025-02-01 00:09:29 +0800

    Update Terminal.cs - Justine
    Pushed changes just in case there were any, because I was fixing basic feature 3

--- PRG ASST/Terminal.cs
+++ PRG ASST/Terminal.cs
@@ -1,76 +1,73 @@
-//==========================================================
-// Student Number: S10268227
-// Student Name: Justine Kyle Supan
-// Partner Name: Jayden Ng
-//==========================================================
+using System;
+using System.Collections.Generic;
 
-namespace PRG_ASST
+public class Terminal
 {
-    public class Terminal
+    public string TerminalName { get; set; }
+    public Dictionary<string, Airline> Airlines { get; set; }
+    public Dictionary<string, Flight> Flights { get; set; } = new Dictionary<string, Flight>();
+    public Dictionary<string, BoardingGate> BoardingGates { get; set; }
+    public Dictionary<string, double> GateFees { get; set; } = new Dictionary<string, double>();
+
+    public Terminal(string terminalName)
     {
-        public string TerminalName { get; set; }
-        public Dictionary<string, Airline> Airlines { get; set; }
-        public Dictionary<string, BoardingGate> BoardingGates { get; set; }
-        public Dictionary<string, double> GateFees { get; set; }
+        TerminalName = terminalName;
+        Airlines = new Dictionary<string, Airline>();
+        BoardingGates = new Dictionary<string, BoardingGate>();
+        GateFees = new Dictionary<string, double>();
+    }
 
-        public Terminal(string terminalName)
+    public bool AddAirline(Airline airline)
+    {
+        try
         {
-            TerminalName = terminalName;
-            Airlines = new Dictionary<string, Airline>();
-            BoardingGates = new Dictionary<string, BoardingGate>();
-            GateFees = new Dictionary<string, double>();
+            Airlines.Add(airline.Code, airline);
+            return true;
         }
-
-        public bool AddAirline(Airline airline)
+        catch
         {
-            if (Airlines.ContainsKey(airline.Code))
-            {
-                return false;
-            }
-            else
-            {
-                Airlines.Add(airline.Code, airline);
-                return true;
-            }
+            Console.WriteLine("Airline Addition Unsuccessful. Do try again. ");
+            return false;
         }
+    }
 
-        public bool AddBoardingGate(BoardingGate boardingGate)
+    public bool AddBoardingGate(BoardingGate boardingGate)
+    {
+        if (BoardingGates.ContainsKey(boardingGate.GateName))
         {
-            if (BoardingGates.ContainsKey(boardingGate.GateName))
-            {
-                return false;
-            }
-            else
-            {
-                BoardingGates.Add(boardingGate.GateName, boardingGate);
-                return true;
-            }
-
-            
+            return false;
         }
-
-        public Airline GetAirlineFromFlight(Flight flight)
+        else
         {
-            foreach (var airline in Airlines.Values)
+            BoardingGates.Add(boardingGate.GateName, boardingGate);
+            return true;
+        }
+    }
+
+    public Airline GetAirlineFromFlight(Flight flight)
+    {
+        foreach (KeyValuePair<string, Airline> kvp in Airlines)
+        {
+            string[] flightno = flight.FlightNumber.Split(' ');
+            if (flightno[0] == kvp.Key)
             {
-                if (airline.Flights.ContainsKey(flight.FlightNumber))
-                    return airline;
+                return kvp.Value;
             }
 
-            return null;
         }
+        return null;
+    }
 
-        public void PrintAirlineFees()
+    public void PrintAirlineFees()
+    {
+        foreach (var airline in Airlines.Values)
         {
-            foreach (var airline in Airlines.Values)
-            {
-                Console.WriteLine($"{airline.Name}: {airline.CalculateFees()}");
-            }
+            Console.WriteLine($"{airline.Name}: {airline.CalculateFees()}");
         }
+    }
 
-        public override string ToString()
-        {
-            return $"Terminal: {TerminalName}, Airlines: {Airlines.Count}, Boarding Gates: {BoardingGates.Count}";
-        }
+    public override string ToString()
+    {
+        return $"Terminal: {TerminalName}, Airlines: {Airlines.Count}, Boarding Gates: {BoardingGates.Count}";
     }
 }
