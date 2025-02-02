0293a18caeb641014927007c0d7dff3f930c16d8
Jayden Ng <167403709+jaydenngjh@users.noreply.github.com>
2025-02-02 21:11:01 +0800

    FINAL CFFTFlight.cs - jn

--- PRG ASST/CFFTFlight.cs
+++ PRG ASST/CFFTFlight.cs
@@ -5,18 +5,18 @@
 //==========================================================
 
 class CFFTFlight : Flight
+{
+    public double RequestFee { get; set; }
+    public CFFTFlight(string flightNumber, string origin, string destination, DateTime expectedTime, string status) : base(flightNumber, origin, destination, expectedTime, status)
     {
-        public double RequestFee { get; set; }
-        public CFFTFlight(string flightNumber, string origin, string destination, DateTime expectedTime, string status) : base(flightNumber, origin, destination, expectedTime, status)
-        {
-            RequestFee = 150;
-        }
-        public double CalculateFees()
-        {
-            return base.CalculateFees() + RequestFee;
-        }
-        public override string ToString()
-        {
-            return base.ToString();
-        }
+        RequestFee = 150;
     }
+    public double CalculateFees()
+    {
+        return base.CalculateFees() + RequestFee;
+    }
+    public override string ToString()
+    {
+        return base.ToString() + $"Request Fee: {RequestFee}";
+    }
+}
