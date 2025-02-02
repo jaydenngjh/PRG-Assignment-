7468c4254648c35c2126c6ab30eed53de1e68a1c
Jayden Ng <167403709+jaydenngjh@users.noreply.github.com>
2025-02-02 21:10:40 +0800

    FINAL LWTTFlight.cs - jn

--- PRG ASST/LWTTFlight.cs
+++ PRG ASST/LWTTFlight.cs
@@ -5,21 +5,21 @@
 //==========================================================
 
 class LWTTFlight : Flight
+{
+    public double RequestFee { get; set; }
+
+    public LWTTFlight(string flightNumber, string origin, string destination, DateTime expectedTime, string status) : base(flightNumber, origin, destination, expectedTime, status)
     {
-        public double RequestFee { get; set; }
-
-        public LWTTFlight(string flightNumber, string origin, string destination, DateTime expectedTime, string status) : base(flightNumber, origin, destination, expectedTime, status)
-        {
-            RequestFee = 500;
-        }
-
-        public double CalculateFees()
-        {
-            return base.CalculateFees() + RequestFee;
-        }
-
-        public override string ToString()
-        {
-            return base.ToString() + $"Request Fee: {RequestFee}";
-        }
+        RequestFee = 500;
     }
+
+    public double CalculateFees()
+    {
+        return base.CalculateFees() + RequestFee;
+    }
+
+    public override string ToString()
+    {
+        return base.ToString() + $"Request Fee: {RequestFee}";
+    }
+}
