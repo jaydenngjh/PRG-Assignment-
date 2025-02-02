9409f25b123e7dd1fe49b442ba7fc961da8d07c6
Jayden Ng <167403709+jaydenngjh@users.noreply.github.com>
2025-02-02 21:11:16 +0800

    FINAL DDJBFlight.cs - jn

--- PRG ASST/DDJBFlight.cs
+++ PRG ASST/DDJBFlight.cs
@@ -5,18 +5,19 @@
 //==========================================================
 
 class DDJBFlight : Flight
+{
+    public double RequestFee { get; set; }
+    public DDJBFlight(string flightNumber, string origin, string destination, DateTime expectedTime, string status) : base(flightNumber, origin, destination, expectedTime, status)
     {
-        public double RequestFee { get; set; }
-        public DDJBFlight(string flightNumber, string origin, string destination, DateTime expectedTime, string status) : base(flightNumber, origin, destination, expectedTime, status)
-        {
-            RequestFee = 300;
-        }
-        public double CalculateFees()
-        {
-            return base.CalculateFees() + RequestFee;
-        }
-        public override string ToString()
-        {
-            return base.ToString();
-        }
+        RequestFee = 300;
     }
+    public double CalculateFees()
+    {
+        return base.CalculateFees() + RequestFee;
+    }
+    public override string ToString()
+    {
+        return base.ToString() + $"Request Fee: {RequestFee}";
+    }
+}
+
